b2087facb22be565d709a5d0daaa6e6396fe3b8b
semhar kidane and Thomas Doherty <thomas@>
2018-03-05 21:07:14 -0800

    add count-by.

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,23 @@ $(document).ready(function(){
     });
   });
 });
+
+
+//For count by
+//$(document).ready(function() {
+  $('#input form').submit(function(event) {
+    event.preventDefault ();
+    debugger;
+    var bigNum = parseInt($('input#userInput').val());
+    var multipleNum = parseInt($('input#countBy').val());
+    var multiples = [];
+
+    for (var i = multipleNum; i <= bigNum; i += multipleNum) {
+      if (bigNum % i === 0) {
+        multiples.push(i);
+      };
+
+      $("#output").text(multiples);
+    }
+  });
+});
